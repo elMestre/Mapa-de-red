41e2778b74946398d7dcbffd6bd742b0bb0afe46
Muscardina <vergonzalezsainz@gmail.com>
2024-07-04 13:31:31 +0200

    Imprimir cada elemento del objeto haciendo referencia a sus atributos por separado

--- coches.js
+++ coches.js
@@ -1,2 +1,10 @@
 const coches = require('./coches.json');
-console.log(coches);
+
+coches.forEach(coche => {
+    console.log("----------------------------------")
+    console.log("  " + "Marca:" + coche.marca) //pide acceso a cada atributo por separado
+    console.log("  " + "Modelo:" + coche.modelo)
+    console.log("  " + "Año:" + coche.año)
+    console.log("  " + "Color:" + coche.color)
+    
+})
