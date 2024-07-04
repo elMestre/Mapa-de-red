6af21485d8298caa561bd4cb69b699be5b6e37d0
Carlos Mestre <carmesce@gmail.com>
2024-07-04 13:56:50 +0200

    Mis Miles

--- coches.js
+++ coches.js
@@ -1,2 +1,18 @@
 const coches = require('./coches.json');
-console.log(coches);
+const misiles = require('./misiles.json');
+
+const vehiculos = Object.assign(coches, misiles)
+
+let imprimir = (coche) => {
+    console.log("--------------------------")
+    console.log("  Nombre: " + coche.modelo);
+    console.log("  Marca : " + coche.marca);
+    console.log("  Color : " + coche.color);
+    console.log("  Año   : " + coche.año);    
+}
+
+vehiculos.forEach(vehiculo => { 
+    imprimir(vehiculo) 
+})
+
+console.log("====================")
